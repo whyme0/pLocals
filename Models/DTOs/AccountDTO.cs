545785a4526c25a6d1cb884c6848ee5a22249aaf
whyme0 <sparkjetstudiost@gmail.com>
2024-03-01 12:42:29 +0300

    modified fields

--- Models/DTOs/AccountDTO.cs
+++ Models/DTOs/AccountDTO.cs
@@ -1,6 +1,15 @@
-﻿namespace pLocals.Models.DTOs
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace pLocals.Models.DTOs
 {
     public class AccountDTO
     {
+        public virtual ICollection<Note> Notes { get; set; } = new HashSet<Note>();
+        [DataType(DataType.Text)]
+        public string? Title { get; set; }
+        [DataType(DataType.Text)]
+        public string? Login { get; set; }
+        [DataType(DataType.Password)]
+        public string? Password { get; set; }
     }
 }

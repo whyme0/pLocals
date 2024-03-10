e571272738c495b618ca54f827421c5ec188769a
whyme0 <sparkjetstudiost@gmail.com>
2024-03-10 23:03:23 +0300

    set fields not required

--- Models/DTOs/AccountDTO.cs
+++ Models/DTOs/AccountDTO.cs
@@ -7,15 +7,12 @@ namespace pLocals.Models.DTOs
         public Note? Note { get; set; }
         
         [DataType(DataType.Text)]
-        [Required]
         public string? Title { get; set; }
         
         [DataType(DataType.Text)]
-        [Required]
         public string? Login { get; set; }
         
         [DataType(DataType.Password)]
-        [Required]
         public string? Password { get; set; }
     }
 }

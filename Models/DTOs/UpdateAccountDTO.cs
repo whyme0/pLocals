8a20dcdca626bf5ebc73543ac36adfd9f7e62760
whyme0 <sparkjetstudiost@gmail.com>
2024-03-20 11:06:41 +0300

    each dtp for each action

---
+++ Models/DTOs/UpdateAccountDTO.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace pLocals.Models.DTOs
+{
+    public class UpdateAccountDTO
+    {
+        [DataType(DataType.MultilineText)]
+        public string? NoteText { get; set; }
+
+        [DataType(DataType.Text)]
+        public string? Title { get; set; }
+
+        [DataType(DataType.Text)]
+        public string? Login { get; set; }
+
+        [DataType(DataType.Password)]
+        public string? Password { get; set; }
+    }
+}

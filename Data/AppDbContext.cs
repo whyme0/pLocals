0501a75b2f0a82c426b2c98cb3edc0bc3eb7edf0
whyme0 <sparkjetstudiost@gmail.com>
2024-03-27 21:43:27 +0300

    seed data

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -27,6 +27,14 @@ namespace pLocals.Data
             modelBuilder.Entity<Account>()
                 .Property(a => a.Password)
                 .IsRequired();
+
+            modelBuilder.Entity<Account>().HasData(
+                new { AccountId = 1, Title = "VK", Login = "74958004020", Password = "qwerty12345", NoteText = "Account for work" },
+                new { AccountId = 2, Title = "GMAIL", Login = "example@gmail.com", Password = "qwerty12345", NoteText = "Mail for work" },
+                new { AccountId = 3, Title = "mail.ru", Login = "example@mail.ru", Password = "qwerty12345", NoteText = "Main mail" },
+                new { AccountId = 4, Title = "steam", Login = "steamlogin", Password = "qwerty12345", NoteText = "Games main account" },
+                new { AccountId = 5, Title = "aliexpress", Login = "74958004020", Password = "qwerty12345", NoteText = "" }
+                );
         }
     }
 }

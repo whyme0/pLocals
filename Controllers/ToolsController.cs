6ad4e4f0a3c7616a91c83f092d4939c6ac597bb7
whyme0 <sparkjetstudiost@gmail.com>
2024-06-28 18:02:47 +0300

    create password generator

---
+++ Controllers/ToolsController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using pLocals.Models.DTOs;
+using pLocals.Tools;
+using System.Collections;
+using System.Text.Json;
+
+namespace pLocals.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ToolsController : Controller
+    {
+        private readonly ILogger<AccountController> _logger;
+        private readonly PasswordManager _passwordManager = new();
+        
+        public ToolsController(ILogger<AccountController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        [Route("generate-pwd/")]
+        public IActionResult GeneratePassword([FromQuery] int length, bool withUppercase = true, bool withSpecials = true)
+        {
+            var output = new GeneratedPassword() { Password = _passwordManager.GeneratePassword(length, withUppercase, withSpecials) };
+
+            return Content(JsonSerializer.Serialize(output), "application/json");
+        }
+    }
+}
---
+++ Models/DTOs/GeneratedPassword.cs
@@ -0,0 +1,7 @@
+﻿namespace pLocals.Models.DTOs
+{
+    record class GeneratedPassword
+    {
+        public string Password { get; set; }
+    }
+}

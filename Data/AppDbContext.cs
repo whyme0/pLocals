788068b9922440b7beabcae6bdfbdf4907ba5a42
whyme0 <sparkjetstudiost@gmail.com>
2024-11-16 22:04:10 +0300

    release and debug modes separated

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -28,7 +28,7 @@ namespace pLocals.Data
             modelBuilder.Entity<Account>()
                 .Property(a => a.Password)
                 .IsRequired();
-
+#if DEBUG
             modelBuilder.Entity<Account>().HasData(
                 new { Id = 1, Title = "VK", NormalizedTitle = "vk", Login = "74958004020", Password = "qwerty12345", NoteText = "Account for work" },
                 new { Id = 2, Title = "GMAIL", NormalizedTitle = "gmail", Login = "example@gmail.com", Password = "qwerty12345", NoteText = "Mail for work" },
@@ -36,6 +36,7 @@ namespace pLocals.Data
                 new { Id = 4, Title = "steam", NormalizedTitle = "steam", Login = "steamlogin", Password = "qwerty12345", NoteText = "Games main account" },
                 new { Id = 5, Title = "aliexpress", NormalizedTitle = "aliexpress", Login = "74958004020", Password = "qwerty12345", NoteText = "" }
                 );
+#endif
         }
     }
 }

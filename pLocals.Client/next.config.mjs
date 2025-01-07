69e810a918604d73126246ec9a9947ed922028b2
whyme0 <sparkjetstudiost@gmail.com>
2025-01-07 18:08:05 +0300

    fix api urls

--- pLocals.Client/next.config.mjs
+++ pLocals.Client/next.config.mjs
@@ -2,7 +2,7 @@
 const nextConfig = {
   output: 'standalone',
   env: {
-      NEXT_PUBLIC_API_FOR_CLIENT: "http://localhost:5187"
+      NEXT_PUBLIC_API_FOR_CLIENT: "http://localhost:5187", // not relate to docker's envs
   }
 };
 

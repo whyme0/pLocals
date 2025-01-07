8ab63032205357d736a7a1c04b73d735679ef45d
whyme0 <sparkjetstudiost@gmail.com>
2025-01-07 16:16:54 +0300

    env var fix

--- pLocals.Client/next.config.mjs
+++ pLocals.Client/next.config.mjs
@@ -2,7 +2,8 @@
 const nextConfig = {
   output: 'standalone',
   env: {
-      NEXT_PUBLIC_API_FOR_CLIENT: "http://localhost:5187"
+      NEXT_PUBLIC_API_FOR_CLIENT: "http://localhost:5187",
+      NEXT_PUBLIC_API_URL: "http://localhost:5187"
   }
 };
 
